6a757fa8b48e4fd9e98d2d67802a457ac0accd84
pengjoshua <joshuapeng7@gmail.com>
2016-11-19 17:34:01 -0800

    Add non working version of Express refactor

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,117 +1,51 @@
-/*************************************************************
-You should implement your request handler function in this file.
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-**************************************************************/
-var fs = require('fs');
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+var express = require('express');
+var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
 
-// var directory = './server';
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser());
+var jsonParser = bodyParser.json();
 
-// var body = {
-//   results: [{'username': 'Welcome', 'text': 'hi!', 'room': 'lobby'}]
-// };
+var messages = [];
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+var findRoomMessages = function(room, messages) {
+  var roomMessages = [];
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'application/json';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  if (request.url === '/classes/messages') {
-    if (request.method === 'GET') {
-      fs.readFile(__dirname + '/messages.txt', 'utf-8', function(err, data) {
-        if (err) {
-          response.writeHead(404);
-          response.end();
-        } else {
-          data = JSON.parse('[' + data + ']');
-          response.writeHead(200, headers);
-          response.end(JSON.stringify({ results: data.reverse() }));
-        }
-      });
-    } else if (request.method === 'POST') {
-      request.on('data', function(message) {
-        var message = JSON.parse(message);
-        fs.appendFile(__dirname + '/messages.txt', ',' + JSON.stringify(message), 'utf-8', function(err) {
-          if (err) {
-            response.writeHead(404);
-            response.end();
-          } else {
-            response.writeHead(201, headers);
-            response.end(JSON.stringify(message));
-          }
-        });
-      });
-      // request.on('end', function() {
-      //   response.end('Updated message successfully!');
-      // });
-    } else if (request.method === 'OPTIONS') {
-      response.writeHead(200, headers);
-      response.end();
-    } else {
-      response.writeHead(404, headers);
-      response.end();
+  for (var i = 0; i < messages.length; i++) {
+    if (messages[i].roomname === room) {
+      roomMessages.push(messages[i]);
     }
-  } else {
-    response.writeHead(404, headers);
-    response.end();
   }
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
 
-  // response.end(JSON.stringify({ results: body.results.slice().reverse() }));
+  return roomMessages;
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
+router.get('/classes/messages/', jsonParser, function (req, res) {
+  res.json({results: messages});
+});
 
-module.exports.requestHandler = requestHandler; 
+router.post('/classes/messages/', jsonParser, function(req, res) {
+  var username = req.param('username');
+  var text = req.param('text');
+  var roomname = req.param('roomname');
+
+  messages.push({username: username, text: text, roomname: roomname});
+});
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
+router.get('/classes/:room', function(req, res) {
+  var roomname = req.param('room');
+  var roomMsgs = findRoomMessages(roomname, roomMessages);
+  res.json({results: roomMsgs});
+});
